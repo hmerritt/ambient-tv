aea52bd82087439db2c1de9f42e90b2196eb5271
Harry Merritt <harry@merritt.es>
2020-11-21 22:00:10 +0000

    fade-in backgroundImage once image has loaded

--- app/components/BackgroundImage.js
+++ app/components/BackgroundImage.js
@@ -7,16 +7,29 @@
  */
 
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { Animated, View, Image, StyleSheet } from 'react-native';
 
 const BackgroundImage: (props) => React$Node = ({ src }) => {
+    // Starting image opacity -> 0
+    const imageOpacity = new Animated.Value(0);
+
+    // Once image has loaded
+    // -> animate opacity from 0 -> 1
+    const onImageLoad = () => {
+        Animated.timing(imageOpacity, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+    };
+
     return (
         <View style={styles.container}>
-            <Image
-                style={styles.image}
+            <Animated.Image
                 source={{
                     uri: src,
                 }}
+                style={[styles.image, { opacity: imageOpacity }]}
+                onLoad={onImageLoad}
             />
         </View>
     );
@@ -24,6 +37,11 @@ const BackgroundImage: (props) => React$Node = ({ src }) => {
 
 const styles = StyleSheet.create({
     container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
         flex: 1,
         backgroundColor: 'transparent',
     },
