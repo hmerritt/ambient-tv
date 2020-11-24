ff7b3d8d588ef8081b2f93e1b84150041ceea655
Harry Merritt <harry@merritt.es>
2020-11-24 00:29:50 +0000

    exclude redundant action types from redux-logger

--- app/src/state/index.js
+++ app/src/state/index.js
@@ -1,12 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 
 const middleware = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
-    middleware.push(logger);
+    middleware.push(
+        createLogger({
+            predicate: (getState, action) =>
+                action.type !== 'IMAGE_LOADING_START' &&
+                action.type !== 'IMAGE_LOADING_END',
+        }),
+    );
 }
 
 export default createStore(rootReducer, applyMiddleware(...middleware));
