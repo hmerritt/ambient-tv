4939ec0b33a6149875839fd0639508dba3c3da94
Harry Merritt <harry@merritt.es>
2020-11-30 04:34:13 +0000

    create function to get all files from b2 bucket

--- app-server/lib/models/backblazeModel.js
+++ app-server/lib/models/backblazeModel.js
@@ -9,10 +9,45 @@ const b2 = new B2({
 
 async function GetBucket(bucket_name) {
     await b2.authorize(); // must authorize first
-    let response = await b2.getBucket({ bucketName: bucket_name });
+    const response = await b2.getBucket({ bucketName: bucket_name });
     return response.data.buckets[0];
+}
+
+async function ListBucketFiles(bucket_id) {
+    const files = [];
+    let startFileName = null;
+
+    while (true) {
+        const response = await b2.listFileNames({
+            bucketId: bucket_id,
+            startFileName: startFileName,
+            maxFileCount: 999,
+            delimiter: "",
+            prefix: "",
+        });
+
+        files.push(...response.data.files);
+
+        if (response.data.nextFileName == null) break;
+
+        startFileName = response.data.nextFileName;
+    }
+
+    return files;
+}
+
+async function GetBucketFiles(bucket_name) {
+    // Fetch bucket info
+    const bucket = await GetBucket(bucket_name);
+
+    // Fetch bucket files
+    const files = await ListBucketFiles(bucket.bucketId);
+
+    return files;
 }
 
 module.exports = {
     GetBucket,
+    ListBucketFiles,
+    GetBucketFiles,
 };
