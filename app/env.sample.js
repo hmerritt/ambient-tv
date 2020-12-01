a5a62e31be19fd21b24c15df59c413f6fb382569
Harry Merritt <harry@merritt.es>
2020-12-01 23:53:14 +0000

    create env.js sample

---
+++ app/env.sample.js
@@ -0,0 +1,13 @@
+/**
+ * Ambient TV
+ * https://github.com/hmerritt/ambient-tv
+ */
+
+/*
+ * Environment Variables
+ */
+export default {
+    RSS_URL: '',
+    APP_SERVER_URL: '',
+    FETCH_METHOD: '',
+};
