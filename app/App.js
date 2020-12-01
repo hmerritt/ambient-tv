f81aa81e1be8ab2f3ae3bce215f4f34a0f47deb0
Harry Merritt <harry@merritt.es>
2020-12-01 23:32:08 +0000

    use expo-keep-awake hook to keep the screen awake

--- app/App.js
+++ app/App.js
@@ -6,12 +6,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { useKeepAwake } from 'expo-keep-awake';
 
 import store from './src/state';
 
 import AppActual from './src/AppActual';
 
 export default function App() {
+    // Keep screen awake
+    useKeepAwake();
+
     return (
         <Provider store={store}>
             <StatusBar
--- app/src/AppActual.js
+++ app/src/AppActual.js
@@ -1,28 +1,15 @@
 /**
  * Ambient TV
  * https://github.com/hmerritt/ambient-tv
- *
- * @format
- * @flow strict-local
  */
 
-import React, { useEffect } from 'react';
-import IdleTimerManager from 'react-native-idle-timer';
+import React from 'react';
 
 import BGSlideshow from './components/BackgroundImage/BGSlideshow';
 import Overlay from './components/Overlay';
 import Title from './components/Title';
 
 const AppActual = () => {
-    // Keep screen awake
-    useEffect(() => {
-        IdleTimerManager.setIdleTimerDisabled(true);
-
-        return () => {
-            IdleTimerManager.setIdleTimerDisabled(false);
-        };
-    }, []);
-
     return (
         <>
             <Title />
