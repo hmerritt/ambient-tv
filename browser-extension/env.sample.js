ee5ba01564609c98778cc9b9f5ad71055b9b0d23
Harry Merritt <harry@merritt.es>
2020-12-03 01:18:47 +0000

    create separate env.sample.js for the browser extension
    - use adjusted animation lengths

---
+++ browser-extension/env.sample.js
@@ -0,0 +1,17 @@
+/**
+ * Ambient TV
+ * https://github.com/hmerritt/ambient-tv
+ */
+
+/*
+ * Environment Variables
+ */
+export default {
+  ANIMATION_LONG: 1500,
+  ANIMATION_SHORT: 750,
+  APP_SERVER_URL: "",
+  FETCH_METHOD: "",
+  IMAGE_TIMER: 300000, //  1000 = 1s  //  60000 = 1m // 120000 = 2m
+  OPENWEATHERMAP_KEY: "",
+  RSS_URL: "",
+};
