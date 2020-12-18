29ef1faaca8545ccf0fb61a3cedc577d0a97d897
Harry Merritt <harry@merritt.es>
2020-12-18 23:06:36 +0000

    update Attribution component to use new object prop
    - use background.attribution property
    - remove legacy unsplash integration
    - tweak css

--- app/src/components/BackgroundImage/About/Attribution.js
+++ app/src/components/BackgroundImage/About/Attribution.js
@@ -9,34 +9,16 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
-import Link from '../../Common/Link';
-
 const Attribution = ({ background }) => {
     // Render nothing if no attribution available
-    if (!background.attribution.name) {
+    if (!background.attribution || background.attribution.length === 0) {
         return <></>;
     }
 
     return (
         <>
             <Text style={[styles.text, styles.attribution]} numberOfLines={1}>
-                {background.method === 'unsplash' && (
-                    <>
-                        By{' '}
-                        <Link type="text" url={background.attribution.link}>
-                            {background.attribution.name}
-                        </Link>{' '}
-                        on{' '}
-                        <Link
-                            type="text"
-                            url={`https://unsplash.com/${background.unsplashReferal}`}>
-                            Unsplash
-                        </Link>
-                    </>
-                )}
-                {background.method !== 'unsplash' && (
-                    <>By {background.attribution.name}</>
-                )}
+                <>By {background.attribution}</>
             </Text>
         </>
     );
@@ -45,7 +27,7 @@ const Attribution = ({ background }) => {
 const styles = StyleSheet.create({
     text: {
         width: 350,
-        marginTop: 4,
+        marginTop: 5,
         opacity: 0.8,
         fontFamily: 'Roboto-Medium',
         fontSize: 10,
