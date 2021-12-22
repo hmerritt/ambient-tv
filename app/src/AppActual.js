019445f4e52db114048d4a9185729baf97ffc620
Harry Merritt <harry@merritt.es>
2021-12-22 21:47:47 +0000

    setup bug-catch/react-native

--- app/src/AppActual.js
+++ app/src/AppActual.js
@@ -4,9 +4,11 @@
  */
 
 import React from 'react';
-import { init as bugcatchInit } from '@bug-catch/browser';
+import { init as bugcatchInit, recordEvent } from '@bug-catch/react-native';
 
 import env from '../env';
+import { bugCatchOptions } from './utils/bugCatch';
+
 import BGSlideshow from './components/BackgroundImage/BGSlideshow';
 import Overlay from './components/Overlay';
 import Title from './components/Title';
@@ -14,10 +16,8 @@ import Title from './components/Title';
 // Bugcatch init
 // logs all errors
 if (env.BUGCATCH_ENABLE) {
-    bugcatchInit({
-        base_url: `${env.APP_SERVER_URL}/bug-catch`,
-        release: '1.1.9',
-    });
+    bugcatchInit(bugCatchOptions);
+    recordEvent('appOpen', 'user has opened the app', bugCatchOptions);
 }
 
 const AppActual = () => {
---
+++ app/src/utils/bugCatch.js
@@ -0,0 +1,11 @@
+/**
+ * Ambient TV
+ * https://github.com/hmerritt/ambient-tv
+ */
+import env from '../../env';
+
+export const bugCatchOptions = {
+    base_url: `${env.APP_SERVER_URL}/bug-catch`,
+    release: '1.1.9',
+    logEvents: true,
+};
