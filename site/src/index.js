236c8238ef21d85322e48e0a01059d9aadd81a2b
hmerritt <harry@mrrtt.me>
2022-02-28 20:21:13 +0000

    import custom cra

--- site/src/index.js
+++ site/src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-import App from './App';
+import App from "./App";
+
+import "styles/index.scss";
+
+const appVersion = process.env.REACT_APP_VERSION || "~";
+console.log(`App [Version ${appVersion}]`);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
