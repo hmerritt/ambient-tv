cc28b2fe727809b5ade2d950c54288b76da139ea
hmerritt <harry@mrrtt.me>
2023-08-01 15:46:59 +0100

    feat: wait for fonts to load before rendering anything

--- app/App.js
+++ app/App.js
@@ -8,10 +8,13 @@ import { useFonts } from 'expo-font';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
 import { useKeepAwake } from 'expo-keep-awake';
+import * as SplashScreen from 'expo-splash-screen';
 
 import store from './src/state';
 
 import AppActual from './src/AppActual';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 	// Keep screen awake
@@ -21,6 +24,10 @@ export default function App() {
 	let [fontsLoaded] = useFonts({
 		'Roboto-Medium': require('./src/assets/fonts/Roboto/Roboto-Medium.ttf'),
 	});
+
+	// Wait for fonts to load
+	if (fontsLoaded) SplashScreen.hideAsync();
+	else return null;
 
 	return (
 		<Provider store={store}>
