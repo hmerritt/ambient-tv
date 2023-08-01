b0216e2d83bad1571598500aa3f382f84da308d9
hmerritt <harry@mrrtt.me>
2023-08-02 00:26:32 +0100

    fix: app crash

--- app/App.js
+++ app/App.js
@@ -15,11 +15,21 @@ import store from './src/state';
 
 import AppActual from './src/AppActual';
 
-bootstrap();
+// Show splash screen until fonts are loaded
+SplashScreen.preventAutoHideAsync();
+
+// Hide navigation bar
+NavigationBar.setPositionAsync("absolute");
+NavigationBar.setVisibilityAsync("hidden");
+NavigationBar.setBehaviorAsync("inset-swipe");
+NavigationBar.setBackgroundColorAsync("#00000080"); // `rgba(0,0,0,0.5)`
+setStatusBarHidden(true, "fade");
 
 export default function App() {
 	// Keep screen awake
 	useKeepAwake();
+
+	const visibility = NavigationBar.useVisibility()
 
 	// Hide navigation bar after a few seconds
 	useEffect(() => {
@@ -53,16 +63,4 @@ export default function App() {
 			<AppActual />
 		</Provider>
 	);
-}
-
-const bootstrap = () => {
-	// Show splash screen until fonts are loaded
-	SplashScreen.preventAutoHideAsync();
-
-	// Hide navigation bar
-	NavigationBar.setPositionAsync("absolute");
-	NavigationBar.setVisibilityAsync("hidden");
-	NavigationBar.setBehaviorAsync("inset-swipe");
-	NavigationBar.setBackgroundColorAsync("#00000080"); // `rgba(0,0,0,0.5)`
-	setStatusBarHidden(true, "none");
 }
