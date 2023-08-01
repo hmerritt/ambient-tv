02245a56816342398b96ffa45f561c8e223fda0b
hmerritt <harry@mrrtt.me>
2023-08-01 15:31:27 +0100

    wip: start new expo app and import previous app files

--- app/App.js
+++ app/App.js
@@ -3,33 +3,33 @@
  * https://github.com/hmerritt/ambient-tv
  */
 
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Provider } from 'react-redux';
-import { useKeepAwake } from 'expo-keep-awake';
 import { useFonts } from 'expo-font';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'expo-status-bar';
+import { useKeepAwake } from 'expo-keep-awake';
 
 import store from './src/state';
 
 import AppActual from './src/AppActual';
 
 export default function App() {
-    // Keep screen awake
-    useKeepAwake();
+	// Keep screen awake
+	useKeepAwake();
 
-    // Load fonts
-    let [fontsLoaded] = useFonts({
-        'Roboto-Medium': require('./src/assets/fonts/Roboto/Roboto-Medium.ttf'),
-    });
+	// Load fonts
+	let [fontsLoaded] = useFonts({
+		'Roboto-Medium': require('./src/assets/fonts/Roboto/Roboto-Medium.ttf'),
+	});
 
-    return (
-        <Provider store={store}>
-            <StatusBar
-                hidden={true}
-                translucent
-                backgroundColor="transparent"
-            />
-            <AppActual />
-        </Provider>
-    );
+	return (
+		<Provider store={store}>
+			<StatusBar
+				hidden={true}
+				translucent
+				backgroundColor="transparent"
+			/>
+			<AppActual />
+		</Provider>
+	);
 }
