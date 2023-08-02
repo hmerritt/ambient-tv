8a9f3cd398cef13affd04716108856f5d2713b81
hmerritt <harry@mrrtt.me>
2023-08-02 15:24:41 +0100

    feat: app-server rss video label

--- app-server/lib/controllers/rssController.js
+++ app-server/lib/controllers/rssController.js
@@ -3,26 +3,26 @@ require("dotenv").config();
 const Storage = require("./storageController");
 
 class Rss {
-    constructor() {
-        this.rss = new Storage();
+	constructor() {
+		this.rss = new Storage();
 
-        // Default values
-        this.rss.set("RSS image feed", "title");
-        this.rss.set("RSS image feed", "description");
-        this.rss.set(
-            'xmlns:media="http://search.yahoo.com/mrss/"',
-            "namespaces"
-        );
-        this.rss.set("item", "item");
-        this.rss.set([], "data");
-    }
+		// Default values
+		this.rss.set("RSS image feed", "title");
+		this.rss.set("RSS image feed", "description");
+		this.rss.set(
+			'xmlns:media="http://search.yahoo.com/mrss/"',
+			"namespaces"
+		);
+		this.rss.set("item", "item");
+		this.rss.set([], "data");
+	}
 
-    set(which, value) {
-        this.rss.set(value, which);
-    }
+	set(which, value) {
+		this.rss.set(value, which);
+	}
 
-    get() {
-        let output = `<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+	get() {
+		let output = `<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 <rss version=\"2.0\" ${this.rss.get("namespaces")}>
     <channel>
         <title>${this.rss.get("title")}</title>
@@ -30,27 +30,27 @@ class Rss {
         <description>${this.rss.get("description")}</description>
         `;
 
-        this.rss.get("data").forEach((image, i) => {
-            output += `
+		this.rss.get("data").forEach((asset, i) => {
+			output += `
     <${this.rss.get("item")}>
-        <title>${image.fileName}</title>
-        <link>${image.link}</link>
-        <description>${image.description}</description>
-        <author>${image.attribution}</author>
-        <category>${image.category}</category>
-        <media:title type="plain">${image.description}</media:title>
-        <media:content url="${image.link}" medium="image"/>
+        <title>${asset.fileName}</title>
+        <link>${asset.link}</link>
+        <description>${asset.description}</description>
+        <author>${asset.attribution}</author>
+        <category>${asset.category}</category>
+        <media:title type="plain">${asset.description}</media:title>
+        <media:content url="${asset.link}" medium="${asset.fileName.endsWith('.mp4') ? "video" : "image"} "/>
     </${this.rss.get("item")}>
             `;
-        });
+		});
 
-        output += `
+		output += `
 </channel>
 </rss>
         `;
 
-        return output;
-    }
+		return output;
+	}
 }
 
 module.exports = Rss;
